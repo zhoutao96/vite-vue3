77d373adccdfe9aaa0c706b108ab7fd8e1f249b4
zhoutao96 <192664764@qq.com>
2024-02-22 11:15:21 +0800

    first

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:vue/vue3-essential",
+        "plugin:prettier/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+export default defineConfig({
+  plugins: [vue()],
+  resolve:{
+    alias:{
+      '@':resolve('./src')
+    }
+  },
+  base:'./',
+  server:{
+    port:4000,
+    open:true,
+    cors:true  // 允许跨域
+  }
+})
